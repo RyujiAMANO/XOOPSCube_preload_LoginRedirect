51b72ae3d3bcd5e49a7e38b35863408b7c3a0c32
RyujiAMANO <ryuji@ryus.co.jp>
2014-01-11 15:27:04 +0900

    1st commit
    from http://ryus.co.jp/modules/wordpress/index.php?p=47

---
+++ loginRedirect.class.php
@@ -0,0 +1,23 @@
+< ?php
+/**
+ *
+ * @author Ryuji AMANO
+ *
+ */
+class loginRedirect extends XCube_ActionFilter
+{
+	function _getRedirectUrl()
+	{
+		return XOOPS_URL .'/admin.php';
+	}
+	function preBlockFilter()
+	{
+		$this->mRoot->mDelegateManager->add("Site.CheckLogin.Success”, array(&$this, ‘loginSuccessRedirect’));
+
+	
+	}
+	function loginSuccessRedirect(&$xoopsUser)
+	{
+		$this->mController->executeForward($this->_getRedirectUrl());
+	}
+}
